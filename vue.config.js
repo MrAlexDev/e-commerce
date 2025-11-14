28bc2de4d5f9f944163b56299c44d50d871d36e5
MrAlexDev <ok.khyzhniak@gmail.com>
2025-11-14 19:53:43 +0200

    Configure publicPath for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,17 @@
 // vue.config.js
 const webpack = require('webpack')
 
+// module.exports = {
+//   configureWebpack: {
+//     plugins: [
+//       new webpack.DefinePlugin({
+//         // Отключаем детальный вывод mismatches в продакшн-бандле
+//         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+//       })
+//     ]
+//   }
+// }
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new webpack.DefinePlugin({
-        // Отключаем детальный вывод mismatches в продакшн-бандле
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
-      })
-    ]
-  }
+  publicPath: process.env.NODE_ENV === 'production' ? '/e-commerce/' : '/',
+  configureWebpack: { /* existing config */ }
 }
